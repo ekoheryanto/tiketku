06c597d482f9bc6ddc83dc0b2fab62333b243b46
ekoheryanto <eko.heryanto94@gmail.com>
2019-07-30 19:08:23 +0700

    Add Success registration

--- app/src/main/java/id/fd/eko/tiketku/RegistwoActivity.java
+++ app/src/main/java/id/fd/eko/tiketku/RegistwoActivity.java
@@ -4,16 +4,26 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class RegistwoActivity extends AppCompatActivity {
 
+    Button btn_continue;
     LinearLayout btn_backto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registwo);
 
+        btn_continue = findViewById(R.id.btn_lanjut);
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent lanjut = new Intent(RegistwoActivity.this, SuccesRegistration.class);
+                startActivity(lanjut);
+            }
+        });
         btn_backto = findViewById(R.id.btn_backto);
         btn_backto.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/id/fd/eko/tiketku/SuccesRegistration.java
@@ -0,0 +1,13 @@
+package id.fd.eko.tiketku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuccesRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_succes_registration);
+    }
+}
